922f87fa83c4dced44f30be78e621912a8273e43
wendel.gualberto <wendel@sankhya.com.br>
2025-02-26 13:58:10 -0300

    Deploy PWA para GitHub Pages

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+      caches.open("app-cache").then((cache) => {
+        return cache.addAll([
+          "./",
+          "./index.html",
+          "./styles.css",
+          "./app.js",
+          "./manifest.json",
+          "./icon-192x192.png",
+          "./icon-512x512.png"
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener("fetch", (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
