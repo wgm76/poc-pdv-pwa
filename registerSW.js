11de8ade8e9c35d308225974360483a77a38aa54
wendel.gualberto <wendel@sankhya.com.br>
2025-02-27 10:54:48 -0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/poc-pdv-pwa/sw.js', { scope: '/poc-pdv-pwa/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
